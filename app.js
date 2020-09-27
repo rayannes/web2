b94f16bb6f8cf80618aea2c47d4a0ebb4a2776d6
rayannesilva <56279350+rayannesilva@users.noreply.github.com>
2020-09-27 09:38:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express()
+const autorRouter = require('./autorRouter')
+var handlebars = require('express-handlebars');
+app.engine('handlebars',handlebars({defaultLayout:'mainLayout'}))
+app.set('view engine', 'handlebars') 
+
+app.get('/autor/detalhe/:nome',  autorRouter.detalheProduto)
+
+
+
+  
+app.listen(3000, () =>{//testando
+	console.log('funcionando')
+})
---
+++ autorRouter.js
@@ -0,0 +1,10 @@
+
+//centralizador de rotas questao um item dois
+
+function detalheProduto(req, res) {
+	//res.type('html')
+	//res.send('detalhe ' + req.params.codigo)
+	res.render('autorDetalhe',req.params.id)
+}
+
+module.exports = {detalheProduto}
